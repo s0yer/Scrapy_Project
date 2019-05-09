36b5e6780c7392477d226f1c87695b830a2d0571
jadsonmarliere <jds.mdo@gmail.com>
2019-05-08 22:57:04 -0300

    implementation

--- Functions.py
+++ Functions.py
@@ -1,5 +1,6 @@
 from scrapy import *
 
+
 class BrickSetSpider(scrapy.Spider):
 	name = "brickset_spider"
 	start_urls = ['http://brickset.com/sets/year-2016']
@@ -19,3 +20,11 @@ class BrickSetSpider(scrapy.Spider):
 					  'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
 					  'image': brickset.css(IMAGE_SELECTOR).extract_first(),
 			}
+
+			NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+			next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+			if next_page:
+				yield scrapy.Request(
+					response.urljoin(next_page),
+					callback=self.parse
+				)
